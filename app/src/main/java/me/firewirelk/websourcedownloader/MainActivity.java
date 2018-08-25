d5dea431758a1f87921f5a10c449f8a14a13426f
manoj1995madushanka <madushankamanoj414@gmail.com>
2018-08-25 23:52:24 +0530

    gui done

--- app/src/main/java/me/firewirelk/websourcedownloader/MainActivity.java
+++ app/src/main/java/me/firewirelk/websourcedownloader/MainActivity.java
@@ -18,5 +18,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DownloadTask dt=new DownloadTask();
+        dt.execute();
     }
 }
