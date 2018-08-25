b80e5588478e554e3c7da19ef2115c1fcff57e28
manoj1995madushanka <madushankamanoj414@gmail.com>
2018-08-26 04:10:17 +0530

    error resolve

--- app/src/main/java/me/firewirelk/websourcedownloader/MainActivity.java
+++ app/src/main/java/me/firewirelk/websourcedownloader/MainActivity.java
@@ -3,14 +3,52 @@ package me.firewirelk.websourcedownloader;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txtURL;
+    Button btnGetresource;
 
     public class DownloadTask extends AsyncTask<String,Void,String>{
 
         @Override
-        protected String doInBackground(String... strings) {
-            return null;
+        protected String doInBackground(String... urls) {
+
+            String result="";
+            URL url;
+            HttpURLConnection urlConnection=null;
+
+            try{
+
+                url=new URL(urls[0]);
+                urlConnection=(HttpURLConnection) url.openConnection();
+
+                InputStream in=urlConnection.getInputStream();
+                InputStreamReader reader=new InputStreamReader(in);
+                int data=reader.read();
+                while(data != -1){
+                    char current=(char)data;
+                    result+=current;
+                    data=reader.read();
+                }
+
+                return "success";
+
+
+            }catch (Exception e){
+                e.printStackTrace();
+                return "error";
+            }
+
+
         }
     }
 
@@ -19,7 +57,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        txtURL=(TextView)findViewById(R.id.txtURL);
+        btnGetresource=(Button)findViewById(R.id.btnGetsource);
+
         DownloadTask dt=new DownloadTask();
-        dt.execute();
+        String result=null;
+        try {
+            result=dt.execute(txtURL.getText().toString()).get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
     }
 }
