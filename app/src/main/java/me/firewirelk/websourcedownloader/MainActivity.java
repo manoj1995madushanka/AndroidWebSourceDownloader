efde972743083cd4d5dba79f856c4e02b85dff3c
manoj1995madushanka <madushankamanoj414@gmail.com>
2018-08-25 23:36:46 +0530

    inner class

--- app/src/main/java/me/firewirelk/websourcedownloader/MainActivity.java
+++ app/src/main/java/me/firewirelk/websourcedownloader/MainActivity.java
@@ -1,9 +1,18 @@
 package me.firewirelk.websourcedownloader;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    public class DownloadTask extends AsyncTask<String,Void,String>{
+
+        @Override
+        protected String doInBackground(String... strings) {
+            return null;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
