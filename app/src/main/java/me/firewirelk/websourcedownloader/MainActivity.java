7a46eb53a1330fac6a6bad5db6177bec7feaf689
manoj1995madushanka <madushankamanoj414@gmail.com>
2018-08-26 11:40:04 +0530

    some exception can not handle me

--- app/src/main/java/me/firewirelk/websourcedownloader/MainActivity.java
+++ app/src/main/java/me/firewirelk/websourcedownloader/MainActivity.java
@@ -1,11 +1,18 @@
 package me.firewirelk.websourcedownloader;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -40,11 +47,25 @@ public class MainActivity extends AppCompatActivity {
                     data=reader.read();
                 }
 
-                return "success";
+                //data=ed1.getText().toString();
+                try {
+                    FileOutputStream fOut = openFileOutput(String.valueOf(url),MODE_WORLD_READABLE);
+                    fOut.write(result.getBytes());
+                    fOut.close();
+                    Toast.makeText(getBaseContext(),"file saved",Toast.LENGTH_SHORT).show();
+                }
+                catch (Exception e) {
+
+                    e.printStackTrace();
+                }
+
+
+                return result;
 
 
             }catch (Exception e){
                 e.printStackTrace();
+                Toast.makeText(getBaseContext(),"file does not saved",Toast.LENGTH_SHORT).show();
                 return "error";
             }
 
@@ -52,14 +73,8 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        txtURL=(TextView)findViewById(R.id.txtURL);
-        btnGetresource=(Button)findViewById(R.id.btnGetsource);
-
+    public void btnClick(View view){
         DownloadTask dt=new DownloadTask();
         String result=null;
         try {
@@ -69,5 +84,19 @@ public class MainActivity extends AppCompatActivity {
         } catch (ExecutionException e) {
             e.printStackTrace();
         }
+        Log.i("Result",result);
+
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtURL=(TextView)findViewById(R.id.txtURL);
+        btnGetresource=(Button)findViewById(R.id.btnGetsource);
+
+
     }
 }
